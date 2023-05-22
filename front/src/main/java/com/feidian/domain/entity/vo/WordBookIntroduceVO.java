4cedc95c44a8de6ac6632e0232e8fb0055a63b52
space dandy <fsdkgldjg>
2023-05-22 11:46:54 +0800

    版本2.0

--- front/src/main/java/com/feidian/domain/entity/vo/WordBookIntroduceVO.java
+++ front/src/main/java/com/feidian/domain/entity/vo/WordBookIntroduceVO.java
@@ -1,2 +1,19 @@
-package com.feidian.domain.entity.vo;public class WordBookIntroduceVO {
+package com.feidian.domain.entity.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WordBookIntroduceVO {
+    private Long id;
+    private String name;
+    private String intro;
+    private String suitableUsers;
+    private Date gmtCreate;
+    private Date gmtModified;
 }
